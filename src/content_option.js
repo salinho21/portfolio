b18af84362769008131e1c17bd3f5dc626155dca
salinho21 <a86617@alunos.uminho.pt>
2022-05-24 18:34:13 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,126 @@
+const logotext = "GONÇALO";
+const meta = {
+    title: "Gonçalo Nogueira",
+    description: "I’m Gonçalo Nogueira computer student _ Taking masters deegre at University of Minho.",
+};
+
+const introdata = {
+    title: "I’m Gonçalo Nogueira",
+    animated: {
+        first: "I love coding",
+        second: "I code cool responsive websites",
+        third: "I made some full-stack web apps",
+        forth: "I like to manage databases",
+    },
+    description: "I’m Gonçalo Nogueira computer student _ Taking masters deegre at University of Minho.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I’m Gonçalo Nogueira computer student _ Taking masters deegre at University of Minho.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "Java",
+        value: 75,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "SQL",
+        value: 70,
+    },
+    {
+        name: "Haskell",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Web app for a car sharing rides.",
+        link: "https://github.com/salinho21/VRUM",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Production and importing of questions to evaluate students of the LEONARDO platform",
+        link: "https://github.com/salinho21/prodQuestoes",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "A multi sport betting platform Vue app.",
+        link: "https://github.com/salinho21/RASBet",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Platform called MajorEventManager to keep track of cultural events happening near the user built with .NET framework.",
+        link: "https://github.com/salinho21/MajorEventManager",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "g1.nogueira.slb@hotmail.com",
+    YOUR_FONE: "(+351 912918445)",
+    description: "",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/salinho21",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/gon%C3%A7alo-nogueira-745864234/",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
